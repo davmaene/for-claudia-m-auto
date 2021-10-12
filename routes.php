2db43725f107b81f753c4808abb599ed3df86a54
davmaene <davidmened@gmail.com>
2021-10-12 20:49:26 +0200

    up init commit for claudia

---
+++ routes.php
@@ -0,0 +1,18 @@
+<?php 
+    // session_start();
+    // i will check my connexion session here
+    $page = null;
+    $defaultpage = "home"; // default page dav
+    $pages = scandir("./pages/"); // scan dir for all pages dav
+
+    if(isset($_GET['page'])) {
+        $page = $_GET['page'];
+        $page .= ".php";
+    }else{
+        $page = $defaultpage;
+        $page .= ".php";
+    }
+
+    if(in_array($page, $pages, true)) $page = $page;
+    else $page = $defaultpage;
+?>
